2dfcb4c40c1703e15b76303bf950768480585947
Nick Mather <nick.mather@respondent.io>
2025-06-05 11:29:17 +1000

    complete tutorial 02

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -10,14 +10,35 @@ import {
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { useForm } from "@tanstack/react-form";
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
+import { AlertCircle, LoaderCircle } from "lucide-react";
+import { validateUsername } from "@/api/user";
+import { z } from "zod";
+
+// zod schema can be used as a validator
+const usernameSchema = z
+  .string()
+  .min(3, "Username must be at least 3 characters long");
 
 export const SignUp = () => {
+  // declaring default values sets types
   const form = useForm({
     defaultValues: {
       username: "",
       password: "",
+      confirmPassword: "",
+    },
+    validators: {
+      // validators can be set at the form level as well as the field level
+      onSubmit: ({ value }) => {
+        if (!value.username || !value.password) {
+          // string error applies to the entire form
+          return "Please fill in all fields";
+        }
+      },
     },
     onSubmit: ({ value }) => {
+      // value is typed automatically
       console.log(value);
     },
   });
@@ -37,26 +58,42 @@ export const SignUp = () => {
             e.stopPropagation();
           }}
         >
+          {/* field is a wrapper component. `name` is typed to the key of the defaultvalue object */}
           <form.Field
             name="username"
+            // validators has props that determine when validation occurs
             validators={{
+              // async needed for debounce
               onChangeAsyncDebounceMs: 500,
-              onChangeAsync: ({ value }) =>
-                value.length < 3 &&
-                "Username must be at least 3 characters long",
+              // simulate async validation - returns invalid if username is 'foo', 'bar' or 'baz'
+              onChangeAsync: ({ value }) => validateUsername(value),
+              // sync validation can be used together with async validation
+              // this will run before the async validation, & prevent the async validation if the sync validation is invalid
+              onChange: usernameSchema, // using zod schema
             }}
+            // form UI goes in the children prop of the field component
             children={(field) => (
+              // field is used to control the component
               <div>
                 <Label htmlFor="username">Username</Label>
-                <Input
-                  id="username"
-                  type="text"
-                  value={field.state.value}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                />
+                <div className="relative">
+                  <Input
+                    id="username"
+                    type="text"
+                    value={field.state.value}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                  {/* getMeta() is used to access the meta data of the field. It gets computed or derived state, as opposed to just accessing field.state.meta directly*/}
+                  {field.getMeta().isValidating && (
+                    <div className="absolute right-2 top-1/2 transform -translate-y-1/2">
+                      <LoaderCircle className="animate-spin" />
+                    </div>
+                  )}
+                </div>
+                {/* field.state.meta.errors is an array of errors that are returned from the field  */}
                 {field.state.meta.errors && (
                   <div className="text-red-500 text-sm mt-1">
-                    {field.state.meta.errors}
+                    {field.state.meta.errors.join(", ")}
                   </div>
                 )}
               </div>
@@ -95,18 +132,61 @@ export const SignUp = () => {
                 />
                 {field.state.meta.errors && (
                   <div className="text-red-500 text-sm mt-1">
+                    {field.state.meta.errors.join(", ")}
+                  </div>
+                )}
+              </div>
+            )}
+          />
+          <form.Field
+            name="confirmPassword"
+            validators={{
+              // every time a field in this array changes, the onChange will be called
+              // this is important to make sure that changes to both fields are validated together
+              onChangeListenTo: ["password"],
+              // fieldApi is used to read the password field
+              onChange: ({ value, fieldApi }) =>
+                value !== fieldApi.form.getFieldValue("password") &&
+                "Passwords do not match",
+            }}
+            children={(field) => (
+              <div>
+                <Label htmlFor="confirmPassword">Confirm Password</Label>
+                <Input
+                  id="confirmPassword"
+                  type="password"
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {field.state.meta.errors && (
+                  <div className="text-red-500 text-sm mt-1">
                     {field.state.meta.errors}
                   </div>
                 )}
               </div>
             )}
           />
+          {/* form.Subscribe is used to subscribe to the form state. 
+          This means that when the form state changes, the component will re-render. */}
+          <form.Subscribe
+            selector={(state) => state.errors}
+            children={(errors) =>
+              errors.length > 0 && (
+                <Alert variant={"destructive"}>
+                  <AlertCircle className="h-4 w-4" />
+                  <AlertTitle>Error</AlertTitle>
+                  <AlertDescription>{errors}</AlertDescription>
+                </Alert>
+              )
+            }
+          />
         </form>
       </CardContent>
       <CardFooter className="flex justify-between">
-        <Button variant="outline" onClick={form.reset}>
+        <Button variant="outline" onClick={() => form.reset()}>
           Reset
         </Button>
+        {/* form methods can be used outside the form component. */}
         <Button onClick={form.handleSubmit}>Sign Up</Button>
       </CardFooter>
     </Card>
